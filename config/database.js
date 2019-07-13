c4dd1c30bb29af51aa410ad522ea8641b88852bd
Herverson <herverson.sousa@gmail.com>
2019-07-13 16:21:56 -0300

    Readme

--- config/database.js
+++ config/database.js
@@ -2,9 +2,8 @@ knexfile = require('../knexfile')
 const knex = require('knex')(knexfile)
 module.exports = knex
 
-knex('users').insert({
-  nome: 'herver',
-  email: 'herver@teste.com.br',
-  password: 'teste'
-  
-}).then(data => console.log(data))
+// knex('users').insert({
+//   nome: 'herver',
+//   email: 'herver@teste.com.br',
+//   password: 'teste'
+// }).then(data => console.log(data))
