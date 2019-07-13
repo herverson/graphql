e60f63d901439f039259fb3069ac0efff93016ee
Herverson <herverson.sousa@gmail.com>
2019-07-13 15:18:10 -0300

    Setup projeto

---
+++ config/database.js
@@ -0,0 +1,10 @@
+knexfile = require('../knexfile')
+const knex = require('knex')(knexfile)
+module.exports = knex
+
+knex('users').insert({
+  nome: 'herver',
+  email: 'herver@teste.com.br',
+  password: 'teste'
+  
+}).then(data => console.log(data))
